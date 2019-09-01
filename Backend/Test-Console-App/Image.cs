e369bdf3dd247abbe5397d540ec2f4db19587f4f
CautiousDev <36693901+CautiousDev@users.noreply.github.com>
2019-09-01 21:47:19 +1000

    Fix: Get album info and photos from Facebook.
    Ability to get all albums on a Facebook page. Retrieves and deserializes album id, name, description and highest quality version of album photos.

---
+++ Backend/Test-Console-App/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Console_App
+{
+    public class Image
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+        public string source { get; set; }
+
+        public Image(int height, int width, string source)
+        {
+            this.height = height;
+            this.width = width;
+            this.source = source;
+        }
+    }
+}
--- Backend/Test-Console-App/Program.cs
+++ Backend/Test-Console-App/Program.cs
@@ -1,4 +1,5 @@
 using RestSharp;
+using Newtonsoft;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -6,7 +7,9 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
-
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Test_Console_App
 {
@@ -19,34 +22,92 @@ namespace Test_Console_App
        
 
         static void Main(string[] args)
-        {
+        {            
             var client = new RestClient("https://graph.facebook.com"); // New client.
             var request = new RestRequest(Method.GET);
-            var accessToken = "EAASbCzWmSTUBANgZBi9Xpf3C6JK6q3AttASyNgvUr1tik3vj59w71IQceLLzkT6EwZB1NhCgHA9MvdryquPmEGBXS4d2YwqAbhT9N14NYgZA032SyeN3ypOYz63oRn6H9669Qa15OzMAxVx8pmeXKsHQeQa9fPy6FwFIhoBfMZB729fG18s3GzZAZBRsReB9UZD";
+            var pageAccessToken = "EAASbCzWmSTUBAH2D80AOXZANhHYVqkc7acQZCqCzGgO4ZAyaUZB0l03MlzXlalnMe5tnV06B2LjR8RJVGKZAD0RRBRlwnbdMXMVYUeBhzFIYnKxvbpHZB4DmN8yQBGxrBjUzuZB1mK7J5uSOdzG2jNhiQI0QqZBUqpUX0AETOQqxvuxyzMWyqpdX";
 
-            request.Resource = "{version}/{campaign_id}/albums";
+            // Main request body.
+            request.Resource = "{version}/me/albums"; // Request template.
             request.AddParameter("version", "v4.0", ParameterType.UrlSegment);
-            request.AddParameter("campaign_id", "1889029911374206", ParameterType.UrlSegment);
-            request.AddParameter("fields", "['name', 'id']");
-            request.AddParameter("access_token", accessToken);
+           
+            // What to get + token for verification (needs renewing every 2 months).
+            request.AddParameter("fields", "['photos{images}','name','description']"); // Fields to get.
+            request.AddParameter("access_token", pageAccessToken);  // Pass access token for page.
 
             var response = client.Execute(request);
-            Console.WriteLine(response.Content);
            
 
-                Console.ReadKey();
-        }
+            
 
 
-     
-        
-      
-        
+
+
+
+
+            // TO DO: SAVE THUMBNAIL VER OF IMG TO IMG CLASS AS THUMBNAIL IMAGE.
+           // using (StreamReader r = new StreamReader("file.json"))
+           // {
+           //     string json = r.ReadToEnd();
+                JObject pageJson = JObject.Parse(response.Content);
+
+                // Get albums and deserialize to Album class objects.
+                List<JToken> jAlbums = pageJson["data"].Children().ToList(); // Album jtokens.
+                List<Album> albums = new List<Album>(); // Album objects to deserialize to.
+                foreach(JToken album in jAlbums)
+                {
+                    try
+                    {
+                        string id = album.SelectToken("id").ToString();
+                        string name = album.SelectToken("name").ToString();
+                        string desc = album.SelectToken("description").ToString();
+
+                        albums.Add(new Album(id, name, desc));
+                    }
+                    catch (Exception e) { }
+                }
+
+                
+
+               
+               
+                // Iterate through all albums.
+                for (int i = 0; i < albums.Count; i++)
+                {                    
+                    // Get photos inside album (each photo contains multiple versions of the Image).
+                    List<JToken> jPhotos = pageJson.SelectToken("data["+i+"].photos.data").ToList(); // Num of photos.                  
+                    List<Image> images = new List<Image>(); 
+                                       
+                    for(int j = 0; j < jPhotos.Count; j++)
+                    {
+                        // Save attributes of best quality image ([0]) in current photo (j) of the current album (i).
+                        int height = (int)pageJson.SelectToken("data[" + i + "].photos.data[" + j + "].images[0].height"); 
+                        int width = (int)pageJson.SelectToken("data[" + i + "].photos.data[" + j + "].images[0].width");
+                        string source = (string)pageJson.SelectToken("data[" + i + "].photos.data[" + j + "].images[0].source");
+
+                        images.Add(new Image(height, width, source));
+                    }
+
+                    albums[i].images = images;
+                }
+
+                Console.WriteLine("ID:: " + albums[0].id);
+                Console.WriteLine("NAME:: " + albums[0].name);
+                Console.WriteLine("DESCRIPTION:: " + albums[0].description);
+                Console.WriteLine("NUM PHOTOS:: " + albums[0].images.Count);
+       //    }
+
+
+
+            Console.ReadKey();
+      }
     }
+   
 
+
+
+    
 
 
    
-
-  
 }
