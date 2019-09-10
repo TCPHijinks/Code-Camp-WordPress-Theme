f08ae3dd58bf85bd703a87d644a0a356d3410476
CautiousDev <36693901+CautiousDev@users.noreply.github.com>
2019-09-11 04:10:46 +1000

    Main Backend.
    Basic db with camp table. Ability to edit, delete and create new camps via url (e.g. "https://.../camps/create"). Also removed unessisary test version.

---
+++ CodingOnCountry/Migrations/20190910172701_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingOnCountry.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Camp",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CampCommunity = table.Column<string>(nullable: true),
+                    CampAddress = table.Column<string>(nullable: true),
+                    EmbededFacebookAlbumLink = table.Column<string>(nullable: true),
+                    CampDate = table.Column<DateTime>(nullable: false),
+                    CampStartTime = table.Column<DateTime>(nullable: false),
+                    CampEndTime = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Camp", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Camp");
+        }
+    }
+}
---
+++ CodingOnCountry/Models/Camp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingOnCountry.Models
+{
+    public class Camp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       
+        public string CampCommunity { get; set; }
+        public string CampAddress { get; set; }
+
+        public string EmbededFacebookAlbumLink { get; set; }
+        
+        [Display(Name = "Camp Date")]
+        [DataType(DataType.Date)]
+        public DateTime CampDate { get; set; }
+
+        [Display(Name = "Camp Start Time")]
+        [DataType(DataType.Time)]
+        public DateTime CampStartTime { get; set; }
+
+        [Display(Name = "Camp End Time")]
+        [DataType(DataType.Time)]
+        public string CampEndTime { get; set; }
+
+        
+      
+    }
+}
