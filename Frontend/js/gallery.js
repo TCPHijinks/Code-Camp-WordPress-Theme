9af1f3a279141efd89f8bd5fe9b978b92dc4a247
Dijana Duricic <dduricic@outlook.com>
2019-09-13 10:20:08 +1000

    Update Gallery
    Each Camp in the list now links to a unique gallery, which is displayed upon clicking the view gallery button.

--- Frontend/js/gallery.js
+++ Frontend/js/gallery.js
@@ -1,14 +1,30 @@
 function selectGallery() {
-    var c = document.getElementById("blogContainer");
-    var g = document.getElementById("campGallery");
+    var c = document.getElementById("recentContainer");
+    var g = document.getElementById("localGalleries");
     c.style.display = "none";
     g.style.display = "block";
 }
 
 function revertGallery() {
-    var c = document.getElementById("blogContainer");
-    var g = document.getElementById("campGallery");
+    var c = document.getElementById("recentContainer");
+    var g = document.getElementById("localGalleries");
     g.style.display = "none";
     c.style.display = "grid";
 }
+
+
+function gallerySelect(gallery) {
+    var g = document.getElementById(gallery);
+
+    g.style.display = "block";
+
+    
+}
+
+function galleryDeselect(gallery) {
+    var g = document.getElementById(gallery);
+
+    g.style.display = "none";
+}
+
 
--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -10,7 +10,9 @@ function closeWarning() {
 }
 
 
-
-
 closeWarning();
+
+
+
+
 
