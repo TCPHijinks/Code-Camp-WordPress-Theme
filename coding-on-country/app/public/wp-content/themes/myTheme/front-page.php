53ae9fde80db034ae14960ab0b5387ed5d8652b7
PhylacteryLost <chodge.iam@gmail.com>
2020-03-15 15:48:18 +1000

    Enqueued bootstrap js, css & jquery
    Bootstrap css and js now usable via functions.php alongside jquery. Now
    all pages can use the styling and js features dynamically.

--- coding-on-country/app/public/wp-content/themes/myTheme/front-page.php
+++ coding-on-country/app/public/wp-content/themes/myTheme/front-page.php
@@ -1,3 +1,5 @@
 <?php get_header(); // Load header by calling WP func ?> 
-
+<div class="container">
+    <h1>Hello from front-page.php</h1>
+</div>
 <?php get_footer(); // Load footer by calling WP func ?> 
