37174c75bd8867cf87a0a6737da0b6589d0a25dc
PhylacteryLost <chodge.iam@gmail.com>
2020-03-19 11:54:23 +1000

    Fixes missing page titles and add basic searching
    Fixes how the page.php template was not showing the page title. Also
    adds basic search functionality to the homepage. Enabling users to
    search for keywords across all pages and posts.

--- coding-on-country/app/public/wp-content/themes/myTheme/archive.php
+++ coding-on-country/app/public/wp-content/themes/myTheme/archive.php
@@ -1,18 +1,26 @@
 <?php get_header(); // Load secondary header by calling WP func w/ that param ?> 
 
 <div class="page-wrap">
-    <div class="container">  
-    <h1><?php echo single_tag_title( ); ?></h1>
-        <?php get_template_part('includes/section','archive'); ?>   <!-- Loop that gens content -->
-
-        <!-- Pagination - only show so many posts at once -->
-        <?php 
-            // Pagination pages.
-            previous_posts_link();
-            next_posts_link();
-        ?>
-      
+<div class="container">  
+<section class="row">
+    <!-- Widget Sidebar -->
+    <div class="col-lg-3">
+        <?php if( is_active_sidebar( 'blog-sidebar' )):?>
+            <?php dynamic_sidebar( 'blog-sidebar' ); ?>
+        <?php endif; ?>
     </div>
+    <div class="col-lg-9">
+        <h1><?php echo single_tag_title( ); ?></h1>
+            <?php get_template_part('includes/section','archive'); ?>   <!-- Loop that gens content -->
+
+            <!-- Pagination - only show so many posts at once -->
+            <?php 
+                // Pagination pages.
+                previous_posts_link();
+                next_posts_link();
+            ?>
+    </div>
+</div>
 </div>
       
 <?php get_footer(); // Load footer by calling WP func ?> 
--- coding-on-country/app/public/wp-content/themes/myTheme/single.php
+++ coding-on-country/app/public/wp-content/themes/myTheme/single.php
@@ -10,7 +10,7 @@
             class="img-fluid mb-3 img-thumbnail">
         <?php endif;?>
 
-        <h1><?php the_title();?></h1></h1>
+        <h1><?php the_title();?></h1>
         <?php get_template_part('includes/section','blogcontent'); ?>          
     </div>
 </div>
