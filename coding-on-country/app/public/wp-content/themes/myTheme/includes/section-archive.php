4c08eb17a15b1f4017ad31a64437b775e3089b5a
PhylacteryLost <chodge.iam@gmail.com>
2020-03-22 11:58:16 +1000

    Adds basic text overlay to future camps
    Adds a basic text overlay to upcoming camp badges on the homepage when
    mouse hover.

--- coding-on-country/app/public/wp-content/themes/myTheme/includes/section-archive.php
+++ coding-on-country/app/public/wp-content/themes/myTheme/includes/section-archive.php
@@ -42,9 +42,18 @@
                 <div class="col-md-4">
                 <div class="card mb-4 box-shadow">                    
                 <!-- Card Image -->
-                <img class="card-img-top" src="<?php echo $thumbnail_url ?>" 
                 
-                alt="<?php the_title();?>">
+                <div class="hover hover-2 text-white rounded">
+                <img class="card-img-top" src="<?php echo $thumbnail_url ?>" alt="<?php the_title();?>">
+                <div class="hover-overlay"></div>
+                    <div class="hover-2-content px-5 py-4">
+                        <h3 class="hover-2-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">Image </span>Caption</h3>
+                        <p class="hover-2-description text-uppercase mb-0">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.</p>
+                    </div>
+                </div>
+                
+              
+                
 
                 <!-- Card Contents -->
                 <div class="card-body">
