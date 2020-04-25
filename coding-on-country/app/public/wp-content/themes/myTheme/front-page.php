ca641980f36d7565257dc5cf7c560c8b139b88a6
PhylacteryLost <chodge.iam@gmail.com>
2020-04-25 16:54:41 +1000

    Moves admin blogs under main content
    Moves admin blogs to be under camps, code quests and the support section.

--- coding-on-country/app/public/wp-content/themes/myTheme/front-page.php
+++ coding-on-country/app/public/wp-content/themes/myTheme/front-page.php
@@ -10,53 +10,49 @@
 
             $content = apply_filters('the_content', $page_data->post_content); 
             $title = $page_data->post_title; 
-            echo "<h1 id=\"$title\"><br><br>$title</h1>"; 
+            echo "<h1 style='text-align:center' id=\"$title\"><br><br>$title</h1>"; 
             echo $content; ?>
 
-</div>
 
-        <div class="row">
-            <div class="col-xl-12 adminPosts">   
-           
-                <?php 
-                    $adminPosts = get_posts();                
-                    foreach ($adminPosts as $singleAPost){                       
-                
-                ?>
-
-                <div class= "adminPost">
-                <a href="<?php echo get_permalink($singleAPost); ?>">
-                <?php echo get_the_post_thumbnail($singleAPost, 'full' , array( 'class' => 'postThumb' ) ); ?>
-                
-                    <div class="aPostText">
-                    <!-- Render Homepage title -->
-                        <h4>
-                        <?php   echo $singleAPost->post_title;  ?></h4>
-
-                        <!-- Render Homepage content -->
-                        <p><?php echo $singleAPost->post_content;?></p>
-                    </div>
-                    </a>
-                </div> <!-- end adminPost div -->
-
-
-                <?php } ?>  
-
-                
-
-            </div>
-        </div>
+   
           
             
         <!-- Render past & upcoming camp badges -->
        
         <?php get_template_part('includes/section','archive'); ?>   <!-- Loop that gens content -->
 
+        <!-- Load all admin blog posts -->
+        </div>
+            <div class="row">
+                <div class="col-xl-12 adminPosts">   
+            
+                    <?php 
+                        $adminPosts = get_posts();                
+                        foreach ($adminPosts as $singleAPost){                       
+                    
+                    ?>
 
+                    <div class= "adminPost">
+                    <a href="<?php echo get_permalink($singleAPost); ?>">
+                    <?php echo get_the_post_thumbnail($singleAPost, 'full' , array( 'class' => 'postThumb' ) ); ?>
+                    
+                        <div class="aPostText">
+                        <!-- Render Homepage title -->
+                            <h4>
+                            <?php   echo $singleAPost->post_title;  ?></h4>
+
+                            <!-- Render Homepage content -->
+                            <p><?php echo $singleAPost->post_content;?></p>
+                        </div>
+                        </a>
+                    </div> <!-- end adminPost div -->
+                <?php } ?>  
+            </div>
+        </div>
        
         
         
-        
+        <!-- Load all normal pages following the camps and code quets -->
         <?php
         $pages = get_pages(); 
                 foreach ($pages as $page_data) {
@@ -67,17 +63,11 @@
                     <?php
                     $content = apply_filters('the_content', $page_data->post_content); 
                     $title = $page_data->post_title; 
-                    echo "<h1 id=\"$title\"><br><br>$title</h1>"; 
+                    echo "<h1 style='text-align:center' id=\"$title\"><br><br>$title</h1>"; 
                     echo $content; ?>
 
                 </div>
-                    <?php
-
-                }
-                
-                
-                ?>
-
+                <?php } ?>
     </div>   
 </div>
 
